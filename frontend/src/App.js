836c68427c7be00023664a7e7d5249e7f18effb4
intern2_crosscut <sourav024saha@gmail.com>
2023-11-25 23:29:04 +0530

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import Header from './components/Header';
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen';
+import {Outlet} from 'react-router-dom';
+
+const App=() =>{
+return (
+  <>
+  <Header/>
+  <main className="py-3">
+   <Container>
+   <Outlet/>
+  
+  </Container>
+ 
+  </main>
+  <Footer/> 
+  </>
+);
+};
+export default App;
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,37 @@
+import {Row,Col} from 'react-bootstrap';
+import {useEffect,useState} from 'react';
+import Product from '../components/Product';
+import axios from 'axios';
+
+
+const HomeScreen =() =>{
+  const [products,setProducts]= useState([]);
+
+
+  useEffect(() =>{
+    const fetchProducts= async() =>{
+       const {data} =await axios.get('/api/products');
+       setProducts (data);
+    };
+     fetchProducts();
+  },[]);
+
+
+
+
+    return(
+        <>
+        <h1> Latest Products!</h1>
+        <Row>
+        { products.map((product) =>(
+            <Col sm={12} md={6}  lg={4} xl={3}>
+            <Product product ={product} />
+            </Col>
+    )) }
+        </Row>
+        
+        </>
+    );
+}
+
+export default HomeScreen;
