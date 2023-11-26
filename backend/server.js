ed35baf3d7311aa364cc558ffa9dce3a539eb64e
intern2_crosscut <sourav024saha@gmail.com>
2023-11-27 01:26:34 +0530

    Fourth Commit

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ app.get('/',(req,res) =>{
 });
 
 
-app.get('/products', (req,res) =>{
-    res.json(products);
-})
+app.get('/api/products', (req,res) =>{
+ res.json(products);
+ })
 app.get('/api/products/:id',(req,res)  =>{
     const product= products.find((p) => p._id === req.params.id);
     res.json(product);
