a4f13eabacce9ea801abe230049a6bc89e7dbace
intern2_crosscut <sourav024saha@gmail.com>
2023-12-14 23:50:37 +0530

    Order Page

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -10,52 +10,10 @@ export const orderApiSlice = apiSlice.injectEndpoints({
         body: order,
       }),
     }),
-    getOrderDetails: builder.query({
-      query: (id) => ({
-        url: `${ORDERS_URL}/${id}`,
-      }),
-      keepUnusedDataFor: 5,
-    }),
-    payOrder: builder.mutation({
-      query: ({ orderId, details }) => ({
-        url: `${ORDERS_URL}/${orderId}/pay`,
-        method: 'PUT',
-        body: details,
-      }),
-    }),
-    getPaypalClientId: builder.query({
-      query: () => ({
-        url: PAYPAL_URL,
-      }),
-      keepUnusedDataFor: 5,
-    }),
-    getMyOrders: builder.query({
-      query: () => ({
-        url: `${ORDERS_URL}/mine`,
-      }),
-      keepUnusedDataFor: 5,
-    }),
-    getOrders: builder.query({
-      query: () => ({
-        url: ORDERS_URL,
-      }),
-      keepUnusedDataFor: 5,
-    }),
-    deliverOrder: builder.mutation({
-      query: (orderId) => ({
-        url: `${ORDERS_URL}/${orderId}/deliver`,
-        method: 'PUT',
-      }),
-    }),
-  }),
-});
-
+})
+})
+   
 export const {
-  useCreateOrderMutation,
-  useGetOrderDetailsQuery,
-  usePayOrderMutation,
-  useGetPaypalClientIdQuery,
-  useGetMyOrdersQuery,
-  useGetOrdersQuery,
-  useDeliverOrderMutation,
+  useCreateOrderMutation
+  
 } = orderApiSlice;
