fd9a2a22b246d95210b78cd3c48b44d7794eb9db
intern2_crosscut <sourav024saha@gmail.com>
2023-12-15 09:26:20 +0530

    Order Screen Resolving

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -22,7 +22,7 @@ const PaymentScreen =() =>{
 const submitHandler =(e) =>{
     e.preventDefault();
     dispatch(savePaymentMethod(paymentMethod));
-    navigate('/placeholder');
+    navigate('/placeorder');
 
 
 }
