044e2119c4f0313209c1e1cb07118af36bcfdc94
intern2_crosscut <sourav024saha@gmail.com>
2024-01-16 23:12:10 +0530

    Final Commit

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
-import CheckoutSteps from '../components/CheckoutSteps';
+import CheckoutSteps from '../components/CheckoutSteps'; 
 import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingScreen = () => {
